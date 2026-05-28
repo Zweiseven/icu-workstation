2eacf9fd2ecca2a8586658b9451acc07909b22f0
Zweiseven <Zweiseven@users.noreply.github.com>
2026-05-28 22:13:47 +0800

    v2.11: 补全检查更新按钮+全线版本号同步+更新日志

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// ICU 工作站 Service Worker v2.6
-// 202605282100
+// 202605282300
 
-const BUILD_VERSION = "202605282100";
+const BUILD_VERSION = "202605282300";
 const CACHE_NAME = "icu-workstation-" + BUILD_VERSION;
 const APP_STATIC = [
   "./styles.css",
